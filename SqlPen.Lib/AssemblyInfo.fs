9482e106043946cfffe6974cbbf81275f6d5312c
Robert Koeninger <rkoeninger@att.net>
2017-06-20 17:32:12 -0400

    Hashed out some code in Generation
    Cleaned up AssemblyInfo

--- SqlPen.Lib/AssemblyInfo.fs
+++ SqlPen.Lib/AssemblyInfo.fs
@@ -3,39 +3,22 @@
 open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
+open Microsoft.FSharp.Core.CompilerServices
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[<assembly: AssemblyTitle("SqlPen.Lib")>]
-[<assembly: AssemblyDescription("")>]
-[<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("SqlPen.Lib")>]
-[<assembly: AssemblyCopyright("Copyright © 2016")>]
-[<assembly: AssemblyTrademark("")>]
-[<assembly: AssemblyCulture("")>]
+[<assembly: AssemblyTitle "SqlPen.Lib">]
+[<assembly: AssemblyDescription "">]
+[<assembly: AssemblyCompany "Robert Koeninger">]
+[<assembly: AssemblyProduct "SqlPen.Lib">]
+[<assembly: AssemblyCopyright "Copyright © Robert Koeninger 2016-2017">]
+[<assembly: AssemblyVersion "1.0.0.0">]
+[<assembly: AssemblyFileVersion "1.0.0.0">]
+[<assembly: ComVisible false>]
+[<assembly: TypeProviderAssembly>]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[<assembly: ComVisible(false)>]
+#if DEBUG
+[<assembly: AssemblyConfiguration "Debug">]
+#else
+[<assembly: AssemblyConfiguration "Release">]
+#endif
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("e989a48d-a4be-465c-abb3-a870c1afc4fd")>]
-
-// Version information for an assembly consists of the following four values:
-// 
-//       Major Version
-//       Minor Version 
-//       Build Number
-//       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
-
-do
-    ()
+do ()
--- SqlPen.Lib/Generation.fs
+++ SqlPen.Lib/Generation.fs
@@ -1,15 +1,60 @@
 ﻿namespace Generation
 
+open System.IO
+open System.Reflection
 open Microsoft.FSharp.Core.CompilerServices
 open Microsoft.FSharp.Collections
 open ProviderImplementation.ProvidedTypes
 
-module Meta =
-    [<assembly:TypeProviderAssembly>]
-    do ()
+// https://github.com/fsprojects/SQLProvider/blob/5f6352e49fee5b743940100c6fb89bb70c62d127/src/SQLProvider/SqlDesignTime.fs
+// https://github.com/mausch/XmlLiteralsTypeProvider/blob/master/XmlLiteralsTypeProvider/XmlLiterals.fs
+// http://gettingsharper.de/2014/12/19/having-fun-with-type-level-numbers-using-a-type-provider/
 
 [<TypeProvider>]
-type QueryProvider(cfg: TypeProviderConfig) as this =
+type QueryProvider(config: TypeProviderConfig) as this =
     inherit TypeProviderForNamespaces()
 
-    //do this.AddNamespace(namespaceName, types)
+    let ns = "SqlPen"
+    let asm = Assembly.GetExecutingAssembly()
+    let tempAsmPath = Path.ChangeExtension(Path.GetTempFileName(), ".dll")
+    let tempAsm = ProvidedAssembly tempAsmPath
+    let loadQueriesProvider = ProvidedTypeDefinition(asm, ns, "LoadQueries", Some typeof<obj>, IsErased = false)
+    let parseQueryProvider = ProvidedTypeDefinition(asm, ns, "ParseQuery", Some typeof<obj>, IsErased = false)
+
+    let addEmptyConstructor (t: ProvidedTypeDefinition) =
+        let ctor = ProvidedConstructor []
+        ctor.InvokeCode <- (fun _ -> <@@ () @@>)
+        t.AddMember ctor
+
+    let loadQueries (t: ProvidedTypeDefinition) (path: string) =
+        ()
+
+    let parseQuery (t: ProvidedTypeDefinition) (text: string) =
+        ()
+
+    do
+        loadQueriesProvider.DefineStaticParameters(
+            [ProvidedStaticParameter("Path", typeof<string>, ".")],
+            fun typeName args ->
+                let path = args.[0] :?> string
+                let templateType = ProvidedTypeDefinition(asm, ns, typeName, Some typeof<obj>, IsErased = false)
+                addEmptyConstructor templateType
+                loadQueries templateType path
+                tempAsm.AddTypes [templateType]
+                templateType
+        )
+
+        parseQueryProvider.DefineStaticParameters(
+            [],
+            fun typeName args ->
+                let text = args.[0] :?> string
+                let templateType = ProvidedTypeDefinition(asm, ns, typeName, Some typeof<obj>, IsErased = false)
+                addEmptyConstructor templateType
+                parseQuery templateType text
+                tempAsm.AddTypes [templateType]
+                templateType
+        )
+
+        this.RegisterRuntimeAssemblyLocationAsProbingFolder config
+        tempAsm.AddTypes [loadQueriesProvider; parseQueryProvider]
+        this.AddNamespace(ns, [loadQueriesProvider; parseQueryProvider])
