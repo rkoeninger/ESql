1890cad927cdb6979e0efb63a8a061c024209c02
Robert Koeninger <rkoeninger@att.net>
2017-06-22 10:19:12 -0400

    Added build and test scripts

---
+++ test.bat
@@ -0,0 +1,3 @@
+nuget install NUnit.ConsoleRunner -Version 3.6.0 -OutputDirectory packages
+
+.\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe --noresult .\Trilogy.Tests\bin\Debug\Trilogy.Tests.dll
