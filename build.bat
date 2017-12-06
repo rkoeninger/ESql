b64ac256cab523dd405b50d32879d01b401d3921
Robert Koeninger <rkoeninger@att.net>
2017-12-06 18:21:55 -0500

    Moved unit tests into their own solution
    Made FSharp.Core a nuget dependency

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
-call msbuild Trilogy.sln
+call msbuild Trilogy.Tests\Trilogy.Tests.sln
 
 call nuget install NUnit.ConsoleRunner -Version 3.6.0 -OutputDirectory packages
 
-call .\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe --noresult .\Trilogy.Tests\bin\Debug\Trilogy.Tests.dll
+call .\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe --noresult Trilogy.Tests\bin\Debug\Trilogy.Tests.dll
 
 call msbuild Trilogy.Demo\Trilogy.Demo.sln
 
